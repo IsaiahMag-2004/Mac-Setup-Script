8b76a8f614945dbdb48b7072c17c484dfd37153d
Isaiah Magana <isaiahmagana@Isaiahs-MacBook-Pro.local>
2023-04-29 20:32:38 -0400

    edit the format of the alias creation

--- create-alias.sh
+++ create-alias.sh
@@ -5,21 +5,35 @@
 
 touch $HOME/.zshrc
 
+echo '#Location based alias' >> $HOME/.zshrc
 echo 'alias desk="cd $HOME/Desktop"' >> $HOME/.zshrc
 echo 'alias down="cd $HOME/Downloads"' >> $HOME/.zshrc
 echo 'alias doc="cd $HOME/Documents"' >> $HOME/.zshrc
 echo 'alias dev="cd $HOME/Developer"' >> $HOME/.zshrc
 echo 'alias home="cd $HOME"' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+
+
 
 
 #Git alias
+echo '#git alias' >> $HOME/.zshrc
 echo 'alias gad="git add"' >> $HOME/.zshrc
 echo 'alias gcm="git commit -m"' >> $HOME/.zshrc
 echo 'alias glg="git log"' >> $HOME/.zshrc
 echo 'alias gst="git status"' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
 
 #brew alias
+echo '#brew alias' >> $HOME/.zshrc
 echo 'alias bs="brew search"' >> $HOME/.zshrc
 echo 'alias bi="brew install"' >> $HOME/.zshrc
 echo 'alias bupd="brew update"' >> $HOME/.zshrc
 echo 'alias bupg="brew upgrade"' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
+echo '  ' >> $HOME/.zshrc
